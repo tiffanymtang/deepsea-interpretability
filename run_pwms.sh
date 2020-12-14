71ea731a876da95e39a4bd451af4186f8b13c8cc
Shuni Li <shuni_li@berkeley.edu>
2020-12-14 06:20:25 +0000

    add pwm extraction script

---
+++ run_pwms.sh
@@ -0,0 +1,14 @@
+# get max activation pwms
+python get_pwm.py --out_tag="max_active_per_seq" --parallel > out/out_files/max_active_per_seq.out
+
+# get max activation pwms
+python get_pwm.py --out_tag="all_active_per_seq" --keep_all_active --parallel > out/out_files/all_active_per_seq.out
+
+# get pwms directly from first layer
+python get_pwm.py --out_tag="direct" --direct > out/out_files/direct.out
+
+# get pwms using only positive test cases (Y = 1)
+python get_pwm.py --out_tag="max_active_per_seq" --parallel --include_y="observed" > out/out_files/max_active_per_seq_yobserved_1.out
+
+# get pwms using only predicted strongly position cases (Yhat > .9)
+python get_pwm.py --out_tag="max_active_per_seq" --parallel --include_y="predicted" > out/out_files/max_active_per_seq_ypredicted_0.9.out
